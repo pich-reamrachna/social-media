c8c217fb5173d504d5542ffeb5d201473748fce2
rachna <pichreamr@gmail.com>
2026-03-23 21:56:10 +0700

    configure ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import prettier from 'eslint-config-prettier';
-import path from 'node:path';
-import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
+import prettier from 'eslint-config-prettier'
+import path from 'node:path'
+import { includeIgnoreFile } from '@eslint/compat'
+import js from '@eslint/js'
+import svelte from 'eslint-plugin-svelte'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import ts from 'typescript-eslint'
+import svelteConfig from './svelte.config.js'
 
-const gitignore_path = path.resolve(import.meta.dirname, '.gitignore');
+const gitignore_path = path.resolve(import.meta.dirname, '.gitignore')
 
 export default defineConfig(
 	includeIgnoreFile(gitignore_path),
@@ -23,6 +23,20 @@ export default defineConfig(
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
 			'no-undef': 'off',
+			'no-var': 'error',
+			'prefer-const': 'error',
+			'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+			'no-debugger': 'error',
+			complexity: ['error', 20],
+			'max-depth': ['error', 3],
+			'no-commented-code/no-commented-code': 'warn',
+			'no-restricted-syntax': [
+				'error',
+				{
+					selector: 'Literal[value=null]',
+					message: 'Use undefined instead of null.'
+				}
+			],
 			'@typescript-eslint/naming-convention': [
 				'error',
 				{
@@ -51,4 +65,4 @@ export default defineConfig(
 			}
 		}
 	}
-);
+)
