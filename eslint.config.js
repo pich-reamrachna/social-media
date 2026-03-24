5c710932ec219c62d60590c54a993a51f0be2e5b
rachna <pichreamr@gmail.com>
2026-03-24 22:54:29 +0700

    Add lefthook

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import path from 'node:path'
 import { includeIgnoreFile } from '@eslint/compat'
 import js from '@eslint/js'
 import svelte from 'eslint-plugin-svelte'
+import noCommentedCode from 'eslint-plugin-no-commented-code'
 import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import ts from 'typescript-eslint'
@@ -29,6 +30,9 @@ export default defineConfig(
 	svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		plugins: {
+			'no-commented-code': noCommentedCode
+		},
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
