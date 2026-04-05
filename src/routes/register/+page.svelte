aa93a8f1d0fa6250832f5fd0e8a3c671c7cfb937
Rachna <pichreamr@gmail.com>
2026-04-05 12:44:09 +0700

    fix: padding and inverted eye symbol

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -208,7 +208,7 @@
 	</div>
 
 	<div
-		class="flex w-full flex-col items-center justify-center bg-[#111111] px-5 py-6 sm:p-12 lg:w-1/2 lg:p-24"
+		class="flex w-full flex-col items-center justify-center bg-[#111111] px-7 py-6 sm:px-10 sm:py-12 lg:w-1/2 lg:p-24"
 	>
 		<div class="w-full max-w-md">
 			<div class="mb-8 sm:mb-10 lg:hidden">
@@ -318,36 +318,12 @@
 							class="w-full rounded-lg border border-gray-800 bg-black px-4 py-3 text-sm text-white placeholder-gray-600 transition-colors focus:border-[#ff5c8d] focus:ring-1 focus:ring-[#ff5c8d] focus:outline-none"
 							required
 						/>
-						{#if password_message}
-							<p
-								class:text-red-400={password_status === 'invalid'}
-								class:text-green-400={password_status === 'valid'}
-								class="mt-2 text-sm break-words"
-							>
-								{password_message}
-							</p>
-						{/if}
 						<button
 							type="button"
 							onclick={toggle_password}
 							class="absolute inset-y-0 right-0 flex items-center pr-4 text-gray-500 transition-colors hover:text-gray-300"
 						>
 							{#if is_show_password}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									class="h-4 w-4"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-									stroke-width="2"
-								>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
-									/>
-								</svg>
-							{:else}
 								<svg
 									xmlns="http://www.w3.org/2000/svg"
 									class="h-4 w-4"
@@ -367,9 +343,33 @@
 										d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
 									/>
 								</svg>
+							{:else}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-4 w-4"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									stroke-width="2"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+									/>
+								</svg>
 							{/if}
 						</button>
 					</div>
+					{#if password_message}
+						<p
+							class:text-red-400={password_status === 'invalid'}
+							class:text-green-400={password_status === 'valid'}
+							class="mt-2 text-sm break-words"
+						>
+							{password_message}
+						</p>
+					{/if}
 				</div>
 
 				<div>
