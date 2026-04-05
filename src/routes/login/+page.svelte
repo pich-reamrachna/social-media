eada231dfafee06e63dba1b9dca683e5ac750962
Rachna <pichreamr@gmail.com>
2026-04-05 12:06:58 +0700

    fix: center 'stay logged in' with checkbox, and fix contradicting eye icon

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -148,7 +148,12 @@
 									<path
 										stroke-linecap="round"
 										stroke-linejoin="round"
-										d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+									/>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
 									/>
 								</svg>
 							{:else}
@@ -163,12 +168,7 @@
 									<path
 										stroke-linecap="round"
 										stroke-linejoin="round"
-										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-									/>
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+										d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
 									/>
 								</svg>
 							{/if}
@@ -184,7 +184,7 @@
 					</div>
 				</div>
 
-				<div class="mt-4 flex items-start gap-3">
+				<div class="mt-4 flex items-center gap-3">
 					<div class="relative flex items-center">
 						<input
 							type="checkbox"
@@ -228,7 +228,7 @@
 			</form>
 
 			<div class="mt-10 text-center text-sm text-gray-500 sm:mt-12">
-				New to the Y?
+				New to Y?
 				<a
 					href={resolve('/register')}
 					class="ml-1 text-[#ff5c8d] transition-colors hover:text-[#ff7eb3]"
